79e5d63d7b711b04f833ad849d9c1a6c0b63b189
JosefMontanari <josef.montanari94@gmail.com>
2024-08-21 17:36:41 +0200

    add images and fonts for frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <p className="test-1">Lord of The Timeline</p>
+      <p className="test-2">Lord of The Timeline</p>
+      <p className="test-3">Lord of The Timeline</p>
+      <p className="test-4">Lord of The Timeline</p>
+    </>
+  );
 }
 
 export default App;
