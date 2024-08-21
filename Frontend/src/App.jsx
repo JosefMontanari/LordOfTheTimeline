1c8087dd6154c57a0a348a8f1011b9b100945ea8
JosefMontanari <josef.montanari94@gmail.com>
2024-08-21 18:07:55 +0200

    add keyframes and updated database with new migration, dont forget to remove existing db and update db

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
       <p className="test-2">Lord of The Timeline</p>
       <p className="test-3">Lord of The Timeline</p>
       <p className="test-4">Lord of The Timeline</p>
+
+      <img className="rotate" src="/ring-text.png" alt="bajs" />
     </>
   );
 }
