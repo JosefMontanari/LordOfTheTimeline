de807cd6561ee885c89b4df66eaec0ca0865deb9
Gustav-Frising <frising96@gmail.com>
2024-08-22 16:20:41 +0200

    Locked-card, Playable-card, False/True-state

--- Frontend/src/Pages/LotrGame/LotrGame.jsx
+++ Frontend/src/Pages/LotrGame/LotrGame.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import LotrCardPlayable from "../../components/LotrCardPlayable/LotrCardPlayable";
+import LotrCardLocked from "../../components/LotrCardLocked/LotrCardLocked";
+import { useState } from "react";
 import "./LotrGame.css";
 
 function LotrGame() {
-  return <div>LotrGame</div>;
+  // För att passa om korten är rätt eller fel när de ändrar state
+  const [cardIsCorrect, setCardIsCorrect] = useState();
+  return (
+    <div>
+    </div>
+  );
 }
 
 export default LotrGame;
