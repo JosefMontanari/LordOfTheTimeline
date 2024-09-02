ddc3a0a1e0520ee4a8f6d48c839f2c7e973cf500
OttJoh94 <145072357+OttJoh94@users.noreply.github.com>
2024-09-02 10:29:45 +0200

    add generic repo and endpoint for easy game

---
+++ Backend/Repos/GenericRepo.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repos
+{
+	public class GenericRepo<T> where T : class
+	{
+		private readonly ApplicationDbContext context;
+		private readonly DbSet<T> dbSet;
+
+		public GenericRepo(ApplicationDbContext context)
+		{
+			this.context = context;
+			dbSet = context.Set<T>();
+		}
+
+		public async Task<List<T>> GetAllAsync()
+		{
+			return await dbSet.ToListAsync();
+		}
+
+		public async Task<T?> GetByIdAsync(int id)
+		{
+			return await dbSet.FindAsync(id);
+		}
+
+		public async Task AddAsync(T model)
+		{
+			await dbSet.AddAsync(model);
+		}
+	}
+}
